fe3d338b6b41938371a06b73ce52fea35266e21b
Simon de Almeida <saimen66@gmail.com>
2017-03-26 19:52:22 -0400

    Fixed errors

--- seneca.test.js
+++ seneca.test.js
@@ -1,6 +1,26 @@
-var seneca = require('./seneca');
+// First require (e.g., load) our seneca.js module
+const seneca = require('./seneca');
 
-test('isValid returns true for simple myseneca address', function() {
-  var simpleEmail = 'someone@myseneca.ca';
-  expect(seneca.isValidEmail(simpleEmail)).toBe(true);
+/**
+ * Tests for seneca.isValidEmail()
+ */
+describe('seneca.isValidEmail()', () => {
+  test('returns true for simple myseneca address', () => {
+    const simpleEmail = 'someone@myseneca.ca';
+    expect(seneca.isValidEmail(simpleEmail)).toBe(true);
+  });
+  test('returns false for a non-myseneca address', () => {
+    const gmailAddress = 'someone@gmail.com';
+    expect(seneca.isValidEmail(gmailAddress)).toBe(false);
+  });
+});
+
+/**
+ * Tests for seneca.formatSenecaEmail()
+ */
+describe('seneca.formatSenecaEmail()', () => {
+  test('adds @myseneca.ca to the end of name', () => {
+    const name = 'mshaw';
+    expect(seneca.formatSenecaEmail(name)).toBe('mshaw@myseneca.ca');
+  });
 });
